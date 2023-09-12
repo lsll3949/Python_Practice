7a5e21cab5fe0532f225ca56e6cd562b4a734408
이정화 <113874154+lsll3949@users.noreply.github.com>
2023-09-12 16:50:26 +0900

    Add files via upload

---
+++ 230911_실습 01.py
@@ -0,0 +1,30 @@
+# 변수에 숫자 대입하고 계산하기
+
+a=1
+b=2
+print(a+b)
+
+# 변수에 문자 대입하고 출력하기
+
+a="Python"
+print(a)
+
+# 조건문 if
+a = 3
+if a > 1:
+    print("a is greeter than 1")
+    
+# 반복문 for
+for a in [1,2,3]:
+    print(a)
+    
+# 반복문 while
+i=0
+while i < 3:
+    i=i+1
+    print(i)
+    
+# 함수
+def add(a,b):
+    return a+b
+    
